12d188d245281526571175fb11f20525ba1562fe
Trevor Fehrman <trevorfehrman@Trevbot-Iota.local>
2018-12-19 16:08:49 -0600

    Typos

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <GloabalStyle />
+        <GlobalStyle />
 
         <Switch>
           <Route
--- client/src/containers/Register.js
+++ client/src/containers/Register.js
@@ -1,1 +1,52 @@
+import React from "react";
+import { connect } from "react-redux";
 
+const Register = () => {
+  const [user, setValue] = useState({
+    username: undefined,
+    email: undefined,
+    password: undefined
+  });
+
+  const [error, setError] = useState(null);
+
+  const handleChange = e => {
+    setValue({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (_.some(user, _.isEmpty))
+      setError("Please complete all of the required fields");
+  };
+
+  return (
+    <Wrapper handleSubmit={handleSubmit} type="Register">
+      <Input
+        name="username"
+        type="text"
+        value={user.username}
+        handleChange={handleChange}
+        placeholder="Please enter your username..."
+      />
+      <Input
+        name="email"
+        type="email"
+        value={user.email}
+        handleChange={handleChange}
+        placeholder="Please enter your email..."
+      />
+      <Input
+        name="password"
+        type="password"
+        value={user.password}
+        handleChange={handleChange}
+        placeholder="Please enter your password..."
+      />
+
+      {error && <p>{error}</p>}
+    </Wrapper>
+  );
+};
+
+export default Register;
