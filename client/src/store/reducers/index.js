4d219a83d64c4c0bf6363431c274699e9a042216
Trevor Fehrman <trevorfehrman@gmail.com>
2018-12-31 14:54:39 -0600

    Quiz Reducer built

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -2,8 +2,9 @@ import { combineReducers } from 'redux';
 
 import authReducer from './authReducer';
 import forumReducer from './forumReducer';
+import quizReducer from './quizReducer';
 
 export default () => {
-    const reducers = combineReducers({authReducer, forumReducer});
+    const reducers = combineReducers({authReducer, forumReducer, quizReducer});
     return reducers;
 }
---
+++ client/src/store/reducers/quizReducer.js
@@ -0,0 +1,51 @@
+
+import {
+	FETCH_ALL_QUIZZES_REQUEST,
+	FETCH_ALL_QUIZZES_FAILURE,
+	FETCH_ALL_QUIZZES_SUCCESS,
+	FETCH_QUIZ_REQUEST,
+	FETCH_QUIZ_SUCCESS,
+	FETCH_QUIZ_FAILURE,
+} from '../actions';
+
+const initialState = {
+	quizzes: undefined,
+	quiz: undefined,
+	loading: false,
+	error: undefined,
+};
+
+const quizReducer = (state = initialState, { payload, type }) => {
+    switch (type) {
+        case FETCH_ALL_QUIZZES_REQUEST:
+			return {
+				...state,
+				loading: true,
+				error: undefined,
+			};
+		case FETCH_ALL_QUIZZES_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				quizzes: payload.filter(quiz => quiz.question_count).sort((a, b) => b.id - a.id),
+			};
+		case FETCH_ALL_QUIZZES_FAILURE:
+			return {
+				...state,
+				loading: false,
+			};
+		case FETCH_QUIZ_REQUEST:
+			return {
+				...state,
+				loading: true,
+			};
+		case FETCH_QUIZ_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				quiz: payload,
+			};
+		default:
+			return state;
+    }
+}
