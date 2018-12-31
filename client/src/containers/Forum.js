30936ae442bcd67b2305f7534b461a01cfaa907e
Trevor Fehrman <trevorfehrman@Trevbot-Iota.local>
2018-12-31 13:18:37 -0600

    Forum Container built

--- client/src/containers/Forum.js
+++ client/src/containers/Forum.js
@@ -2,3 +2,31 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchPosts, fetchPost } from '../store/actions/forumActions';
+import { Post } from '../components/Forum/Post';
+
+const Forum = ({ fetchPosts, fetchPost, posts, loading, ...props }) => {
+    useEffect(
+        () => {
+            if (!posts) {
+                fetchPosts();
+            }
+        },
+        [ posts ],
+    );
+
+    const getPost = id => {
+        fetchPost(id);
+        props.history.push(`forum/${id}`);
+    };
+
+    if (posts) return posts.map(post => <Post post={post} handleClick={() => getPost(post.id)}></Post>);
+    else return <div>Loading...</div>
+
+}
+
+const mapStateToProps = ({ forumReducer }) => ({
+    posts: forumReducer.posts,
+    loading: forumReducer.loading,
+});
+
+export default connect(mapStateToProps, { fetchPosts, fetchPost })(Forum);
